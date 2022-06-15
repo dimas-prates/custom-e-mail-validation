e22ef0922d45aeb68d67b7620d4baf9917369309
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-15 20:31:01 -0300

    :sparkles: feat(main.js): Collect html ellments, prevented submit behavior and add validation logic

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,20 @@
-import IndexFunctions from "./index";
+// import IndexFunctions from "./index";
+//Harvesting elements
+const form = document.querySelector("form"),
+  eInput = form.querySelector(".input"),
+  text = form.querySelector(".text");
+
+form.addEventListener("submit", (res) => {
+  res.preventDefault();
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  form.classList.add("error");
+  form.classList.remove("valid");
+  if (eInput.value == "") {
+    text.innerText = "Email can't be blank";
+  } else if (!eInput.value.match(pattern)) {
+    text.innerText = "Please enter a valid email";
+  } else {
+    form.classList.replace("error", "valid");
+    text.innerText = "This is a valid email";
+  }
+});
